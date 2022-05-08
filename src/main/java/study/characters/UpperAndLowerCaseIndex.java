5eaf3ff10faa87bce7504a625a71174eacf59980
mdo@ukr.net <mdo@ukr.net>
2022-05-08 14:43:42 -0300

    character index added

---
+++ src/main/java/study/characters/UpperAndLowerCaseIndex.java
@@ -0,0 +1,17 @@
+package study.characters;
+
+public class UpperAndLowerCaseIndex {
+
+    public static void main(String[] args) {
+
+        //97
+        System.out.println('a' - 0);
+        //122
+        System.out.println('z' - 0);
+        //65
+        System.out.println('A' - 0);
+        //90
+        System.out.println('Z' - 0);
+
+    }
+}
